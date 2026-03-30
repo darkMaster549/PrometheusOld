b86dfe7ffa75ad5230716a6a60a21bf2f873a3b1
Casuke <kylellamas723@gmail.com>
2026-03-30 14:02:06 +0800

    Message.

--- cli.lua
+++ cli.lua
@@ -1,9 +1,4 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- test.lua
--- This script contains the Code for the Prometheus CLI
-
--- Configure package.path for requiring Prometheus
+-- This file is From Original Prometheus.
 local function script_path()
 	local str = debug.getinfo(2, "S").source:sub(2)
 	return str:match("(.*[/%\\])") or "";
