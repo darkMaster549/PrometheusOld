225fb812610e2021feaf4cc666bc61890d5c490c
Casuke <kylellamas723@gmail.com>
2026-03-30 12:52:36 +0800

    ?

---
+++ src/Kepler/namegenerators.lua
@@ -0,0 +1,6 @@
+return {
+	Mangled = require("Kepler.namegenerators.mangled");
+	MangledShuffled = require("Kepler.namegenerators.mangled_shuffled");
+	Il = require("Kepler.namegenerators.Il");
+	Number = require("Kepler.namegenerators.number");
+}
